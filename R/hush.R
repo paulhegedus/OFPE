42c278e8b63da0cdd41cb0d2792297cc70f414a7
paulhegedus <paulhegedus@montana.edu>
2020-07-10 17:15:46 -0600

    added hush function
    from Danny, see source

---
+++ R/hush.R
@@ -0,0 +1,15 @@
+#' Truly invisible output
+#'
+#' Suppress all output from function return. Useful for st_write. Credit goes
+#' to "Danny", https://stackoverflow.com/users/699385/danny.
+#'
+#' @param code Function or code.
+#' @return Execution of function or code.
+#' @source \url{https://stackoverflow.com/questions/2723034/suppress-output-of-a-function}
+#' @export
+hush <- function(code){
+  sink("NUL") # use /dev/null in UNIX
+  tmp = code
+  sink()
+  return(tmp)
+}
