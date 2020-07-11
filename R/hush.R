68f91b4a996d50ba6b972c447372ac97e3240696
paulhegedus <paulhegedus@montana.edu>
2020-07-11 12:55:23 -0600

    hush to internal fun

--- R/hush.R
+++ R/hush.R
@@ -1,12 +1,11 @@
-#' @title Truly invisible output
+#' @title Truly invisible output (internal)
 #'
 #' @description Suppress all output from function return. Useful for st_write. Credit goes
 #' to "Danny", https://stackoverflow.com/users/699385/danny.
-#'
+#' @keyword Internal
 #' @param code Function or code.
 #' @return Execution of function or code.
 #' @source \url{https://stackoverflow.com/questions/2723034/suppress-output-of-a-function}
-#' @export
 hush <- function(code){
   sink("NUL") # use /dev/null in UNIX
   tmp = code
