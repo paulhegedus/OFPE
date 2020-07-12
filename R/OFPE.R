0fb090b612853aebe7da32bc35163388447594f2
paulhegedus <paulhegedus@montana.edu>
2020-07-12 13:16:47 -0600

    loadExtensions init build

--- R/OFPE.R
+++ R/OFPE.R
@@ -9,6 +9,7 @@
 #' and generation of prescriptive application maps for optimized inputs.
 #'
 #' @importFrom magrittr %>%
+#' @importFrom DBI dbSendQuery
 #'
 #' @seealso \url{https://sites.google.com/site/ofpeframework/}
 #' @seealso \url{https://paulhegedus.github.io/OFPE-Website/}
