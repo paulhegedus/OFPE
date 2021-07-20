db75f932cdbd69829f430385d4d8fa97b0507c00
paulheg <paulhegedus@montana.edu>
2021-07-20 09:45:29 -0600

    Revert "v1.3.14"
    This reverts commit 9b8faf1d84857e89dd4f7e8cd152458c8d0db6c8.

--- data-raw/Billings_PremDock_2016.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `Billings_PremDock_2016` dataset goes here
-
-usethis::use_data(Billings_PremDock_2016, overwrite = TRUE)
--- data-raw/MT_Organic_vs_Conv_wheat_N_prices.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `MT_Organic_vs_Conv_wheat_N_prices` dataset goes here
-
-usethis::use_data(MT_Organic_vs_Conv_wheat_N_prices, overwrite = TRUE)
