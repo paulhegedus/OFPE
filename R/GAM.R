1a343fd65a6f48c50f8955cde56d7919130153a0
paulhegedus <paulhegedus@montana.edu>
2021-12-18 19:28:24 +0000

    v1.7.19
    updated the GAM function to find better initial values of k

--- R/GAM.R
+++ R/GAM.R
@@ -86,14 +86,11 @@ GAM <- R6::R6Class(
       self$expvar <- expvar
       self$covars <- covars
       
-      init_k <- ifelse(nrow(self$dat$trn) < 1000 | nrow(self$dat$val) < 1000,
-                       50,
-                       init_k)
       if (self$expvar %in% self$covars) {
-        covars <- self$covars[-grep(self$expvar, self$covars)]
+        self$covars <- self$covars[-grep(self$expvar, self$covars)]
       }
       self$parm_df <- data.frame(
-        parms = c(expvar, covars),
+        parms = c(self$expvar, self$covars),
         k = init_k,
         bad_parms = FALSE,
         means = NA,
@@ -178,79 +175,66 @@ GAM <- R6::R6Class(
       ## brute force method for finding a reasonable 'k' estimate.
       good_parms <- self$parm_df[!self$parm_df$bad_parms, "parms"]
       for (i in 1:length(good_parms)) {
-        unq_vals <- unique(
-          self$dat$trn[, which(names(self$dat$trn) %in% as.character(self$parm_df$parms[i])),
-              with = FALSE][[1]],
-          na.rm = TRUE
-        )
-        if (length(unq_vals) < 5) {
-          self$parm_df$k[i] <- 2
-        } else {
-          if (length(unq_vals) < 10) {
-            self$parm_df$k[i] <- 5
+        tryK <- c(10, 5, 3, 2, 1)
+        for (j in 1:length(tryK)) {
+          if (!exists("foundK")) { foundK <- FALSE }
+          # if foundK  = FALSE (have not found a k that fits, keep trying)
+          if (!foundK) {
+            # set the k in the paramter table to the k estimate
+            self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                              self$parm_df$parms), "k"] <- tryK[j]
+            # make the function statement
+            fxn <- private$.makeFormula(
+              self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                                self$parm_df$parms), "parms"],
+              self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                                self$parm_df$parms), "k"]
+            )
+            # fit model with the estimated k
+            rand_rows <- runif(nrow(self$dat$trn) * 0.25, 1, nrow(self$dat$trn) + 1) %>%
+              as.integer()
+            tryCatch(
+              {mgcv::bam(as.formula(fxn),
+                         data = self$dat$trn[rand_rows, ])
+                foundK <- TRUE },
+              warning = function(w) {foundK <- FALSE },
+              error = function(e) {foundK <- FALSE })
+            # if the model was fit then foundK = T & k in self$parm_df table
+            # otherwise is FALSE and will try the next k
+          }
+        } # end tryK
+        # if no k found
+        if (!foundK) {
+          if (grepl("^aa_n$", self$parm_df$parms[i]) |
+              grepl("^aa_sr$", self$parm_df$parms[i])) {
+            self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                              self$parm_df$parms), "bad_parms"] <- FALSE
+            self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                              self$parm_df$parms), "k"] <- 5
           } else {
-            # trgtK <- self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                            self$parm_df$parms), "k"]
-            # tryK <- c(trgtK,
-            #           ifelse(round(trgtK / 2) > 20,
-            #                  round(trgtK / 2),
-            #                  20),
-            #           ifelse(round(trgtK / 4) > 15,
-            #                  round(trgtK / 4),
-            #                  15),
-            #           ifelse(round(trgtK / 10) > 10,
-            #                  round(trgtK / 10),
-            #                  10),
-            #           ifelse(round(trgtK / 20) > 5,
-            #                  round(trgtK / 20),
-            #                  5),
-            #           3, 2, 1
-            # )
-            # for (j in 1:length(tryK)) {
-            #   if (!exists("foundK")) { foundK <- FALSE }
-            #   # if foundK  = FALSE (have not found a k that fits, keep trying)
-            #   if (!foundK) {
-            #     # set the k in the paramter table to the k estimate
-            #     self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                       self$parm_df$parms), "k"] <- tryK[j]
-            #     # make the function statement
-            #     fxn <- private$.makeFormula(
-            #       self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                         self$parm_df$parms), "parms"],
-            #       self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                         self$parm_df$parms), "k"]
-            #     )
-            #     # fit model with the estimated k
-            #     rand_rows <- runif(nrow(self$dat$trn) * 0.25, 1, nrow(self$dat$trn) + 1) %>%
-            #       as.integer()
-            #     tryCatch(
-            #       {mgcv::bam(as.formula(fxn),
-            #                  data = self$dat$trn[rand_rows, ])
-            #         foundK <- TRUE },
-            #       warning = function(w) {foundK <- FALSE },
-            #       error = function(e) {foundK <- FALSE })
-            #     # if the model was fit then foundK = T & k in self$parm_df table
-            #     # otherwise is FALSE and will try the next k
-            #   }
-            # } # end tryK
-            # # if no k found
-            # if (!foundK) {
-            #   if (grepl("^aa_n$", self$parm_df$parms[i]) |
-            #       grepl("^aa_sr$", self$parm_df$parms[i])) {
-            #     self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                       self$parm_df$parms), "bad_parms"] <- FALSE
-            #     self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                       self$parm_df$parms), "k"] <- 5
-            #   } else {
-            #     self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                       self$parm_df$parms), "bad_parms"] <- TRUE
-            #     self$parm_df[grep(paste0("^", good_parms[i], "$"),
-            #                       self$parm_df$parms), "k"] <- NA
-            #   }
-            # }
-            # rm(foundK) # remove the indicator for the next var in loop
+            self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                              self$parm_df$parms), "bad_parms"] <- TRUE
+            self$parm_df[grep(paste0("^", good_parms[i], "$"),
+                              self$parm_df$parms), "k"] <- NA
           }
         }
+        rm(foundK) # remove the indicator for the next var in loop
+        
+        
+        # unq_vals <- unique(
+        #   self$dat$trn[, which(names(self$dat$trn) %in% as.character(good_parms[i])),
+        #       with = FALSE][[1]],
+        #   na.rm = TRUE
+        # )
+        # if (length(unq_vals) <= 5) {
+        #   self$parm_df$k[i] <- length(unq_vals) - 1
+        # } else {
+        #   if (length(unq_vals) <= 10 & length(unq_vals) > 5) {
+        #     self$parm_df$k[i] <- 5
+        #   } else {
+        #     
+        #   }
+        # }
       } # end parms
     },
     .makeFormula = function(parms = NULL, K = NULL, BS = "ts", xyK = 20) {
@@ -264,7 +248,7 @@ GAM <- R6::R6Class(
       fxn <- paste(
         c(paste0("s(x, y, bs = 'gp', k = ", xyK, ", m = 2)"),
           paste0("s(", parms, ", k = ",
-                 K, ", bs = '", BS, "')")),
+                 K, ", bs = '", BS, "', fx = TRUE)")),
         collapse = " + ")
       fxn <- paste0(self$respvar, " ~ ", fxn)
       return(fxn)
